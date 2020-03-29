76a71255abef05861921b4f3a0afc1a1765b3e2d
Michael Michlin <mmulya@outlook.com>
2020-03-29 12:21:42 -0400

    Fix file list in package.json and upgrade package version to 0.2.8

--- src/mimcssTypes.ts
+++ src/mimcssTypes.ts
@@ -5,9 +5,9 @@ export * from "./styles/ColorTypes";
 export * from "./styles/StyleTypes";
 export * from "./styles/MediaTypes";
 export * from "./styles/FontFaceTypes";
-export * from "./rules/RuleTypes";
 export * from "./styles/SelectorTypes";
 export * from "./styles/tsh";
+export * from "./rules/RuleTypes";
 
 export {Num, Len, Angle, Time, Resolution, Frequency, Percent} from "./styles/UtilFuncs";
 export {useOptimizedStyleNames} from "./rules/StyleScope";
