30c8a5fa113abfc6d8c06f66175f1b1ce2594f34
Michael Michlin <mmulya@outlook.com>
2020-05-12 00:21:22 -0400

    Use testLonghandProp and testShorthandProp in tests.

--- src/test/specs/propsLength.ts
+++ src/test/specs/propsLength.ts
@@ -15,100 +15,60 @@ describe("style properties of type <length>:", () =>
 	{
 		it("padding-left, integer number", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { paddingLeft: 8})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyPropValue( a!.c, "paddingLeft", "8px");
-
-			css.$deactivate( a!);
+			dom.testLonghandProp( "paddingLeft", 8, "8px");
 		})
 
 		it("margin-left, floating point number", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { marginLeft: 0.5})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyPropValue( a!.c, "marginLeft", "0.5em");
-
-			css.$deactivate( a!);
+			dom.testLonghandProp( "marginLeft", 0.5, "0.5em");
 		})
 
 		it("padding, single integer number", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { padding: 8})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyMultiPropValues( a!.c, {
-				paddingTop: "8px",
-				paddingRight: "8px",
-				paddingBottom: "8px",
-				paddingLeft: "8px",
-			});
-
-			css.$deactivate( a!);
+			dom.testShorthandProp( "padding", 8,
+				{
+					paddingTop: "8px",
+					paddingRight: "8px",
+					paddingBottom: "8px",
+					paddingLeft: "8px",
+				}
+			);
 		})
 
 		it("margin, two floating point numbers", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { margin: [0.25, 0.5]})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyMultiPropValues( a!.c, {
-				marginTop: "0.25em",
-				marginRight: "0.5em",
-				marginBottom: "0.25em",
-				marginLeft: "0.5em",
-			});
-
-			css.$deactivate( a!);
+			dom.testShorthandProp( "margin", [0.25, 0.5],
+				{
+					marginTop: "0.25em",
+					marginRight: "0.5em",
+					marginBottom: "0.25em",
+					marginLeft: "0.5em",
+				}
+			);
 		})
 
 		it("padding, three different numbers", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { padding: [8, 6.5, 4]})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyMultiPropValues( a!.c, {
-				paddingTop: "8px",
-				paddingRight: "6.5em",
-				paddingBottom: "4px",
-				paddingLeft: "6.5em",
-			});
-
-			css.$deactivate( a!);
+			dom.testShorthandProp( "padding", [8, 6.5, 4],
+				{
+					paddingTop: "8px",
+					paddingRight: "6.5em",
+					paddingBottom: "4px",
+					paddingLeft: "6.5em",
+				}
+			);
 		})
 
 		it("margin, four different numbers", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { margin: [0.25, 4, "1%", 3]})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyMultiPropValues( a!.c, {
-				marginTop: "0.25em",
-				marginRight: "4px",
-				marginBottom: "1%",
-				marginLeft: "3px",
-			});
-
-			css.$deactivate( a!);
+			dom.testShorthandProp( "margin", [0.25, 4, "1%", 3],
+				{
+					marginTop: "0.25em",
+					marginRight: "4px",
+					marginBottom: "1%",
+					marginLeft: "3px",
+				}
+			);
 		})
 	})
 
@@ -118,28 +78,12 @@ describe("style properties of type <length>:", () =>
 	{
 		it("min", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { width: css.Len.min( "90%", 250, 30.5)})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyPropValue( a!.c, "width", "min(90%, 250px, 30.5em)");
-
-			css.$deactivate( a!);
+			dom.testLonghandProp( "width", css.Len.min( "90%", 250, 30.5), "min(90%, 250px, 30.5em)");
 		})
 
 		it("calc", () =>
 		{
-			class A extends css.StyleDefinition
-			{
-				c = css.$class( { width: css.Len.calc `(90% - ${250}) / 2 + ${2.5}`})
-			}
-
-			let a = css.$activate( A);
-			dom.verifyPropValue( a!.c, "width", "calc((90% - 250px) / 2 + 2.5em)");
-
-			css.$deactivate( a!);
+			dom.testLonghandProp( "width", css.Len.calc`(90% - ${250}) / 2 + ${2.5}`, "calc((90% - 250px) / 2 + 2.5em)");
 		})
 	})
 })
